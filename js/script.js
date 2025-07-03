86252a8fcd35d80ecc5c90b38772dd57e8cb60e1
mahmoudfawzy22 <mahmoudfawzy1100011@gmail.com>
2025-07-03 22:59:44 +0300

    new version

--- js/script.js
+++ js/script.js
@@ -1,57 +1,58 @@
-const boxs = document.querySelectorAll('.features .box');
+// Hero Slider
+const sliderTrack = document.querySelector('.features .slider-track');
+const boxes = document.querySelectorAll('.features .box');
 const leftRow = document.querySelector('.features .left-row');
 const rightRow = document.querySelector('.features .right-row');
 
-const projectImg = document.querySelector(".project .img-content img");
-const leftRowProject = document.querySelector(".project .img-content .left-row");
-const rightRowProject = document.querySelector(".project .img-content .right-row");
+let currentSlide = 0;
 
-const EL_video = document.querySelector("#video");
-const EL_play = document.querySelector(".play-icon");
+function updateSlide() {
+  sliderTrack.style.transform = `translateX(-${currentSlide * 100}vw)`;
+}
 
-EL_play.addEventListener("click", () => {
-  const isPaused = EL_video.paused;
-  EL_video[isPaused ? "play" : "pause"]();
-  EL_video.classList.toggle("u-none", !isPaused);
+leftRow.addEventListener('click', () => {
+  currentSlide = (currentSlide - 1 + boxes.length) % boxes.length;
+  updateSlide();
 });
 
-playVideo();
-const imgs = [
-  "http://127.0.0.1:5500/imgs/project-slide1.jpg",
-  "http://127.0.0.1:5500/imgs/project-slide2.jpg",
-  "http://127.0.0.1:5500/imgs/project-slide3.jpg"
-];
+rightRow.addEventListener('click', () => {
+  currentSlide = (currentSlide + 1) % boxes.length;
+  updateSlide();
+});
 
-function updateBox(direction) {
-  const activeIndex = Array.from(boxs).findIndex(box => box.classList.contains('active'));
-  boxs[activeIndex].classList.remove('active');
+// Project Section Slider
+const projectTrack = document.querySelector('.project-track');
+const leftRowProject = document.querySelector('.project .left-row');
+const rightRowProject = document.querySelector('.project .right-row');
 
-  const newIndex = direction === 'left'
-    ? (activeIndex - 1 + boxs.length) % boxs.length
-    : (activeIndex + 1) % boxs.length;
+let currentProjectSlide = 0;
+const totalProjectSlides = projectTrack.children.length;
 
-  boxs[newIndex].classList.add('active');
-}
-
-leftRow.addEventListener('click', () => updateBox('left'));
-rightRow.addEventListener('click', () => updateBox('right'));
-
-
-function updateImgProject(direction) {
-  const currentSrc = projectImg.src;
-  const activeIndex = imgs.findIndex(img => img === currentSrc);
-
-  if (activeIndex === -1) return;
-
-  let newIndex;
-  if (direction === "left") {
-    newIndex = (activeIndex - 1 + imgs.length) % imgs.length;
+function updateProjectSlide(direction) {
+  if (direction === 'left') {
+    currentProjectSlide = (currentProjectSlide - 1 + totalProjectSlides) % totalProjectSlides;
   } else {
-    newIndex = (activeIndex + 1) % imgs.length;
+    currentProjectSlide = (currentProjectSlide + 1) % totalProjectSlides;
   }
 
-  projectImg.src = imgs[newIndex];
+  const slideWidth = 100 / totalProjectSlides;
+  projectTrack.style.transform = `translateX(-${currentProjectSlide * slideWidth}%)`;
 }
 
-leftRowProject.addEventListener("click", () => updateImgProject("left"));
-rightRowProject.addEventListener("click", () => updateImgProject("right"));
+leftRowProject.addEventListener('click', () => updateProjectSlide('left'));
+rightRowProject.addEventListener('click', () => updateProjectSlide('right'));
+
+// Video Toggle
+const EL_video = document.querySelector('#video');
+const EL_play = document.querySelector('.play-icon');
+
+EL_play.addEventListener('click', () => {
+  const isPaused = EL_video.paused;
+  if (isPaused) {
+    EL_video.classList.remove('u-none');
+    EL_video.play();
+  } else {
+    EL_video.pause();
+    EL_video.classList.add('u-none');
+  }
+});
